9e608cf53488813b83cffad6b7df65490046a839
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-09-11 20:03:49 +0300

    feat: added flutter_intl

--- lib/app_spoosk.dart
+++ lib/app_spoosk.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:spoosk/generated/l10n.dart';
 
 class SpooskApp extends StatelessWidget {
   const SpooskApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
