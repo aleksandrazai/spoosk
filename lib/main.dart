b1e410570ac519a060490ab608408fd9ce40abb0
Oleg <ohlegsss@gmail.com>
2023-09-25 13:58:51 +0300

    feat: added folders for clean arhitecture, remove folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:spoosk/app_spoosk.dart';
-import 'package:spoosk/firebase_options.dart';
+import 'package:spoosk/core/app_spoosk.dart';
+import 'package:spoosk/core/Infrastructure/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Pass all uncaught "fatal" errors from the framework to Crashlytics
