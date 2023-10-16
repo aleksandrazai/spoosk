93a17ca345dff753c86471863359a0c412989cfe
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-10-16 23:38:43 +0300

    feat: added CustomAppBar & assets

--- lib/core/presentation/image.dart
+++ lib/core/presentation/image.dart
@@ -3,3 +3,7 @@ const String image_comparison = 'assets/img/comparison.svg';
 const String image_profile = 'assets/img/profile.svg';
 const String image_search = 'assets/img/search.svg';
 const String image_selected = 'assets/img/selected.svg';
+const String image_header_background = 'assets/img/header_background.svg';
+const String image_header_slogan = 'assets/img/header_slogan.svg';
+const String test_png_header = 'assets/img/PNG_header.png';
+const String image_header_logo = 'assets/img/header_logo.png';
--- lib/core/presentation/screens/home.dart
+++ lib/core/presentation/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spoosk/core/presentation/image.dart';
 
 @RoutePage()
 class Home extends StatefulWidget {
@@ -12,6 +14,73 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SafeArea(
+        child: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: MediaQuery.of(context).size.height * 0.3,
+          floating: false,
+          pinned: true,
+          backgroundColor: const Color(0xF8F8F8),
+          elevation: 0,
+          flexibleSpace: const FlexibleSpaceBar(
+            background: CustomAppBar(),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          width: MediaQuery.of(context).size.width * 0.9,
+          top: 20,
+          child: Image.asset(test_png_header, fit: BoxFit.cover),
+        ),
+        Positioned(
+          width: MediaQuery.of(context).size.width * 0.9,
+          top: 32,
+          child: SvgPicture.asset(image_header_slogan, fit: BoxFit.contain),
+        ),
+        Positioned(
+          top: 28,
+          right: 25,
+          child: Image.asset(
+            image_header_logo,
+            width: 50,
+            height: 16,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomSearchField extends StatelessWidget {
+  const CustomSearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(); //TODO
   }
 }
