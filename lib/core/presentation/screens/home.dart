c2a1d05580b85a6155b5e0cb4bbc5a28a78577c3
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-10-31 20:05:40 +0300

    minor refactor home.dart

--- lib/core/presentation/screens/home.dart
+++ lib/core/presentation/screens/home.dart
@@ -104,21 +104,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-class ReviewButton extends StatelessWidget {
-  final String buttonText;
-  final VoidCallback? onPressedCallback;
-  const ReviewButton({
-    super.key,
-    required this.buttonText,
-    required this.onPressedCallback,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FilledButton(
-      onPressed: onPressedCallback,
-      child: Text(buttonText, style: Theme.of(context).textTheme.bodyLarge),
-    );
-  }
-}
