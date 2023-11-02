eb381b3cbb4da19e67e45715c783da0f5bc323ce
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-11-02 22:26:39 +0300

    feat: added reviewform

--- lib/core/presentation/routes.gr.dart
+++ lib/core/presentation/routes.gr.dart
@@ -8,53 +8,80 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:auto_route/auto_route.dart' as _i10;
+import 'package:flutter/material.dart' as _i11;
+import 'package:spoosk/core/data/models/resorts.dart' as _i12;
+import 'package:spoosk/core/presentation/routes.dart' as _i7;
 import 'package:spoosk/core/presentation/screens/comparison.dart' as _i1;
 import 'package:spoosk/core/presentation/screens/home.dart' as _i2;
 import 'package:spoosk/core/presentation/screens/main_screen.dart' as _i3;
 import 'package:spoosk/core/presentation/screens/profile.dart' as _i4;
-import 'package:spoosk/core/presentation/screens/selected.dart' as _i5;
-import 'package:spoosk/core/presentation/screens/selection.dart' as _i6;
+import 'package:spoosk/core/presentation/screens/resort.dart' as _i5;
+import 'package:spoosk/core/presentation/screens/resort_screen.dart' as _i6;
+import 'package:spoosk/core/presentation/screens/selected.dart' as _i8;
+import 'package:spoosk/core/presentation/screens/selection.dart' as _i9;
 
-abstract class $AppRouter extends _i7.RootStackRouter {
+abstract class $AppRouter extends _i10.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i7.PageFactory> pagesMap = {
+  final Map<String, _i10.PageFactory> pagesMap = {
     Comparison.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.Comparison(),
       );
     },
     Home.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i2.Home(),
       );
     },
     MainRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i3.MainScreen(),
       );
     },
     Profile.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i4.Profile(),
       );
     },
-    Selected.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+    Resort.name: (routeData) {
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.Selected(),
+        child: const _i5.Resort(),
+      );
+    },
+    ResortRoute.name: (routeData) {
+      final args = routeData.argsAs<ResortRouteArgs>();
+      return _i10.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: _i6.ResortScreen(
+          key: args.key,
+          resort: args.resort,
+        ),
+      );
+    },
+    ResortTab.name: (routeData) {
+      return _i10.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i7.ResortTabPage(),
+      );
+    },
+    Selected.name: (routeData) {
+      return _i10.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i8.Selected(),
       );
     },
     Selection.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i10.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i6.Selection(),
+        child: const _i9.Selection(),
       );
     },
   };
@@ -62,8 +89,8 @@ abstract class $AppRouter extends _i7.RootStackRouter {
 
 /// generated route for
 /// [_i1.Comparison]
-class Comparison extends _i7.PageRouteInfo<void> {
-  const Comparison({List<_i7.PageRouteInfo>? children})
+class Comparison extends _i10.PageRouteInfo<void> {
+  const Comparison({List<_i10.PageRouteInfo>? children})
       : super(
           Comparison.name,
           initialChildren: children,
@@ -71,13 +98,13 @@ class Comparison extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Comparison';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i2.Home]
-class Home extends _i7.PageRouteInfo<void> {
-  const Home({List<_i7.PageRouteInfo>? children})
+class Home extends _i10.PageRouteInfo<void> {
+  const Home({List<_i10.PageRouteInfo>? children})
       : super(
           Home.name,
           initialChildren: children,
@@ -85,13 +112,13 @@ class Home extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Home';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i3.MainScreen]
-class MainRoute extends _i7.PageRouteInfo<void> {
-  const MainRoute({List<_i7.PageRouteInfo>? children})
+class MainRoute extends _i10.PageRouteInfo<void> {
+  const MainRoute({List<_i10.PageRouteInfo>? children})
       : super(
           MainRoute.name,
           initialChildren: children,
@@ -99,13 +126,13 @@ class MainRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'MainRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i4.Profile]
-class Profile extends _i7.PageRouteInfo<void> {
-  const Profile({List<_i7.PageRouteInfo>? children})
+class Profile extends _i10.PageRouteInfo<void> {
+  const Profile({List<_i10.PageRouteInfo>? children})
       : super(
           Profile.name,
           initialChildren: children,
@@ -113,13 +140,79 @@ class Profile extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Profile';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.Selected]
-class Selected extends _i7.PageRouteInfo<void> {
-  const Selected({List<_i7.PageRouteInfo>? children})
+/// [_i5.Resort]
+class Resort extends _i10.PageRouteInfo<void> {
+  const Resort({List<_i10.PageRouteInfo>? children})
+      : super(
+          Resort.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'Resort';
+
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i6.ResortScreen]
+class ResortRoute extends _i10.PageRouteInfo<ResortRouteArgs> {
+  ResortRoute({
+    _i11.Key? key,
+    required _i12.Resorts? resort,
+    List<_i10.PageRouteInfo>? children,
+  }) : super(
+          ResortRoute.name,
+          args: ResortRouteArgs(
+            key: key,
+            resort: resort,
+          ),
+          initialChildren: children,
+        );
+
+  static const String name = 'ResortRoute';
+
+  static const _i10.PageInfo<ResortRouteArgs> page =
+      _i10.PageInfo<ResortRouteArgs>(name);
+}
+
+class ResortRouteArgs {
+  const ResortRouteArgs({
+    this.key,
+    required this.resort,
+  });
+
+  final _i11.Key? key;
+
+  final _i12.Resorts? resort;
+
+  @override
+  String toString() {
+    return 'ResortRouteArgs{key: $key, resort: $resort}';
+  }
+}
+
+/// generated route for
+/// [_i7.ResortTabPage]
+class ResortTab extends _i10.PageRouteInfo<void> {
+  const ResortTab({List<_i10.PageRouteInfo>? children})
+      : super(
+          ResortTab.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'ResortTab';
+
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i8.Selected]
+class Selected extends _i10.PageRouteInfo<void> {
+  const Selected({List<_i10.PageRouteInfo>? children})
       : super(
           Selected.name,
           initialChildren: children,
@@ -127,13 +220,13 @@ class Selected extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Selected';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i6.Selection]
-class Selection extends _i7.PageRouteInfo<void> {
-  const Selection({List<_i7.PageRouteInfo>? children})
+/// [_i9.Selection]
+class Selection extends _i10.PageRouteInfo<void> {
+  const Selection({List<_i10.PageRouteInfo>? children})
       : super(
           Selection.name,
           initialChildren: children,
@@ -141,5 +234,5 @@ class Selection extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Selection';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i10.PageInfo<void> page = _i10.PageInfo<void>(name);
 }
--- lib/core/presentation/screens/home.dart
+++ lib/core/presentation/screens/home.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:spoosk/core/presentation/blocs/bloc/request_controller_bloc.dart';
+import 'package:spoosk/core/presentation/image.dart';
 import '../widgets/widgets.dart';
 import 'package:spoosk/core/data/RequestController.dart';
 
@@ -87,10 +90,11 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(
                       horizontal: 24.0, vertical: 8.0),
-                  child: FilledButton(
-                    onPressed: () {},
-                    child: Text('Написать отзыв',
-                        style: Theme.of(context).textTheme.bodyLarge),
+                  child: ReviewButton(
+                    buttonText: 'Написать отзыв',
+                    onPressedCallback: () {
+                      openReviewForm(context);
+                    },
                   ),
                 ),
               ),
@@ -99,6 +103,166 @@ class _HomeScreenState extends State<HomeScreen> {
         }
         return Center(child: CircularProgressIndicator());
       },
+    );
+  }
+
+  Future<dynamic> openReviewForm(BuildContext context) {
+    return showModalBottomSheet(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        context: context,
+        builder: (BuildContext context) {
+          return ReviewForm();
+        });
+  }
+}
+
+class ReviewButton extends StatelessWidget {
+  const ReviewButton(
+      {super.key, required this.onPressedCallback, required this.buttonText});
+  final VoidCallback? onPressedCallback;
+  final String buttonText;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: onPressedCallback,
+      child: Text(buttonText),
+    );
+  }
+}
+
+class ReviewForm extends StatefulWidget {
+  const ReviewForm({super.key});
+
+  @override
+  State<ReviewForm> createState() => _ReviewFormState();
+}
+
+class _ReviewFormState extends State<ReviewForm> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: Column(
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Opacity(
+                  opacity: 0.40,
+                  child: Container(
+                    width: 32,
+                    height: 4,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFF79747E),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  width: 60,
+                  height: 60,
+                  decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      color: Colors.blue),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Роза Хутор',
+                        style: Theme.of(context).textTheme.headlineMedium),
+                    Text('Краснодарский край',
+                        style: Theme.of(context).textTheme.bodyMedium),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Как бы вы оценили курорт?',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                  ],
+                ),
+                RatingBar.builder(
+                    initialRating: 0,
+                    direction: Axis.horizontal,
+                    allowHalfRating: false,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                    itemBuilder: (context, index) {
+                      return SvgPicture.asset(
+                        image_star,
+                        height: 32,
+                        width: 32,
+                      );
+                    },
+                    onRatingUpdate: (rating) {
+                      print(rating);
+                    }),
+                Container(
+                  height: 105,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  color: Color(0xFFF6F6F6),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 310,
+                        child: Text(
+                          'Дополни свою оценку отзывом - это поможет другим райдерам определится с выбором курорта',
+                          style: TextStyle(
+                            color: Color(0xFF9B9CA0),
+                            fontSize: 14,
+                            fontFamily: 'Nunito Sans',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Дополни свой отзыв фотографиями',
+                  style: TextStyle(
+                    color: Color(0xFF4C4E4F),
+                    fontSize: 14,
+                    fontFamily: 'Nunito Sans',
+                    fontWeight: FontWeight.w400,
+                    height: 0.10,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
