26465a4062fda11b8bcecec827968e5efed44275
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-11-09 22:08:22 +0300

    add: model skipass type

---
+++ lib/core/data/models/skipass_type.dart
@@ -0,0 +1,9 @@
+class SkipassType {
+  static Map<String, String> type = {
+    "one_time": "Одноразовый",
+    "one_day": "1",
+    "four_hours": "4 часа",
+    "three_days": "3",
+    "five_days": "5",
+  };
+}
