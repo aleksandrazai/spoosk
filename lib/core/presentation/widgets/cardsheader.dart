d4eb2b0f70656d25b2251e0b85f47d04807985e4
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-11-14 21:45:57 +0300

    fix: refactor cardsheader

--- lib/core/presentation/widgets/cardsheader.dart
+++ lib/core/presentation/widgets/cardsheader.dart
@@ -3,26 +3,25 @@ import 'package:flutter/material.dart';
 class CardsHeaderPopularALL extends StatelessWidget {
   const CardsHeaderPopularALL({
     super.key,
+    required this.header,
+    required this.all,
   });
+
+  final String header;
+  final String all;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16.0),
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            'Популярные курорты',
-            style: TextStyle(
-              color: Color(0xFF4C4E4F),
-              fontSize: 20,
-              fontFamily: 'Nunito Sans',
-              fontWeight: FontWeight.w600,
-            ),
+            header,
+            style: Theme.of(context).textTheme.headlineLarge,
           ),
           Text(
-            'Все', //добавить переход на все курорты
+            all, //добавить переход на все курорты
             style: TextStyle(
               color: Color(0xFF005FF9),
               fontSize: 14,
