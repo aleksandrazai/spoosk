07c68f0e12cf6e3bc2a1261c3441cba835a80592
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-11-19 12:58:21 +0300

    fix: layout changes to region bottomsheet

--- lib/core/presentation/screens/selection_screen/selection_screen_bottomSheet_region.dart
+++ lib/core/presentation/screens/selection_screen/selection_screen_bottomSheet_region.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spoosk/core/presentation/bloc_region/region_bloc.dart';
+import 'package:spoosk/core/presentation/widgets/CustomButtonWithContent.dart';
 import 'package:spoosk/core/presentation/widgets/custom_searchfield.dart';
 
 class SelectionScreenBottomSheetRegion extends StatefulWidget {
@@ -33,59 +34,36 @@ class _SelectionScreenBottomSheetRegionState
             children: [
               Row(
                 children: [
-                  Text(
-                      style: Theme.of(context).textTheme.headlineMedium,
-                      "В каком регионе вы хотите покататься?"),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Text(
+                        style: Theme.of(context).textTheme.headlineMedium,
+                        "В каком регионе вы хотите покататься?"),
+                  ),
                 ],
               ),
               const SizedBox(
                 height: 12,
               ),
-              const CustomSearchField(),
-              SizedBox(
-                height: 200,
-                width: 200,
-                child: ListView.builder(
-                    itemCount: state.regionsAll.length,
-                    itemBuilder: (BuildContext context, index) {
-                      final region = state.regionsAll[index].region;
-                      return CustomButtonSelection(
-                        buttonText: region,
-                        onPressed: () {},
-                      );
-                    }),
+              CustomSearchField(autofocus: true),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Wrap(
+                  spacing: 8,
+                  children: state.regionsAll.map<Widget>((Regions) {
+                    final region = Regions.region;
+                    return CustomButtonFilter(
+                      margin: const EdgeInsets.only(top: 12),
+                      text: region,
+                    );
+                  }).toList(),
+                ),
               ),
             ],
           );
         }
         return const Center(child: CircularProgressIndicator());
       },
-    );
-  }
-}
-
-class CustomButtonSelection extends StatelessWidget {
-  CustomButtonSelection({
-    super.key,
-    required this.buttonText,
-    required this.onPressed,
-  });
-
-  final VoidCallback? onPressed;
-  final String buttonText;
-  Color borderColor = Colors.blue;
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-          side: BorderSide(color: borderColor, width: 0.1),
-        ),
-      ),
-      onPressed: onPressed,
-      child: Text(buttonText),
     );
   }
 }
