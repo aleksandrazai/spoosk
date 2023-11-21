a21587f03133342b9f5fb7b89342a0cb35f9cec3
aleksandrazai <127782384+aleksandrazai@users.noreply.github.com>
2023-11-22 01:56:54 +0300

    feat: added ChangeNotifierProvider

--- lib/core/app_spoosk.dart
+++ lib/core/app_spoosk.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:spoosk/core/data/models/fliter_models.dart/levels.dart';
+import 'package:spoosk/core/data/models/fliter_models.dart/months.dart';
+import 'package:spoosk/core/data/models/fliter_models.dart/regions.dart';
 import 'package:spoosk/core/generated/l10n.dart';
 import 'package:spoosk/core/presentation/bloc_by_id/resort_by_id_bloc.dart';
 import 'package:spoosk/core/presentation/bloc_region/region_bloc.dart';
@@ -19,7 +23,7 @@ class _SpooskAppState extends State<SpooskApp> {
   @override
   Widget build(BuildContext context) {
     AppRouter appRouter = AppRouter();
-    return MultiBlocProvider(
+    return MultiProvider(
         providers: [
           BlocProvider(
             create: (context) => RequestControllerBloc(),
@@ -29,6 +33,15 @@ class _SpooskAppState extends State<SpooskApp> {
           ),
           BlocProvider(
             create: (context) => RegionBloc(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SelectedRegionsModel(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SelectedMonthsModel(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SelectedLevelsModel(),
           ),
         ],
         child: MaterialApp.router(
