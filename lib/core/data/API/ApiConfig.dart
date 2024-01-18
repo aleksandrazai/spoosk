a4e26b9051c86524d457b8256f2685fea5fe3907
Oleg <ohlegsss@gmail.com>
2024-01-18 14:43:46 +0300

    feat: added api key

--- lib/core/data/API/ApiConfig.dart
+++ lib/core/data/API/ApiConfig.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String token = dotenv.env['API_TOKEN']!;
+final String token = dotenv.env['API-key']!;
 final String userToken = dotenv.env['USER_TOKEN']!;
 
 class ApiConfigurateGet {
